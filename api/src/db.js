3d639d252ab0644b1cff3a8fc195f38a79880101
Agustín <agustin.fidelio@gmail.com>
2023-05-28 17:37:17 -0300

    cambio variables entorno

--- api/src/db.js
+++ api/src/db.js
@@ -5,11 +5,11 @@ const path = require('path');
 
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_URL
 } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pfhenry`,
+  DB_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
